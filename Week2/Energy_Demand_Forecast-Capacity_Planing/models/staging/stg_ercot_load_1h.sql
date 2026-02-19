c95ee9c18c395645a7cc622ed1e228ad110a41e4
Smriti-Karanjit <smritikaranjit100@gmail.com>
2026-02-19 02:35:38 +0000

    COMMIT

--- Week2/Energy_Demand_Forecast-Capacity_Planing/models/staging/stg_ercot_load_1h.sql
+++ Week2/Energy_Demand_Forecast-Capacity_Planing/models/staging/stg_ercot_load_1h.sql
@@ -1,17 +1,19 @@
--- models/staging/stg_ercot_load_1h.sql
--- Output: one row per hour (DST duplicates averaged)
+{{ config(materialized='view') }}
 
-CREATE OR REPLACE VIEW ercot.stg_ercot_load_1h AS
-SELECT
-  hour_ts,
-  AVG(ercot_mw) AS ercot_mw,
-  AVG(coast_mw) AS coast_mw,
-  AVG(east_mw)  AS east_mw,
-  AVG(fwest_mw) AS fwest_mw,
-  AVG(north_mw) AS north_mw,
-  AVG(ncent_mw) AS ncent_mw,
-  AVG(south_mw) AS south_mw,
-  AVG(scent_mw) AS scent_mw,
-  AVG(west_mw)  AS west_mw
-FROM ercot.stg_ercot_load
-GROUP BY hour_ts;
+-- Hourly ERCOT load (exported from Databricks CE, loaded via dbt seed)
+
+select
+    cast(hour_ts as timestamp) as hour_ts,
+
+    cast(ercot_mw as double) as ercot_mw,
+    cast(coast_mw as double) as coast_mw,
+    cast(east_mw  as double) as east_mw,
+    cast(fwest_mw as double) as fwest_mw,
+    cast(north_mw as double) as north_mw,
+    cast(ncent_mw as double) as ncent_mw,
+    cast(south_mw as double) as south_mw,
+    cast(scent_mw as double) as scent_mw,
+    cast(west_mw  as double) as west_mw
+
+from {{ ref('ercot_load_1h') }}
+where hour_ts is not null
